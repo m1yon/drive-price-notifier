57353eb2fa8d90d6b017ad3c786d2dd09d8cfbf4
Michael Lyon <mlyon360@gmail.com>
2024-01-24 15:58:50 -0700

    seperate function out

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,7 @@
-import * as cheerio from "cheerio";
+import scrapePrices from "./utils/scrapePrices";
 
 const main = async () => {
-  const html = await fetch("https://diskprices.com").then((res) => res.text());
-  const $ = cheerio.load(html);
-
-  const prices = $("tr.disk")
-    .toArray()
-    .map((el) => {
-      const $el = $(el);
-
-      return {
-        name: $(el).find(".name").text(),
-        productType: $(el).attr("data-product-type"),
-        pricePerGB:
-          Number($($el.children().get(0)).text().replace("$", "")) ?? undefined,
-        pricePerTB:
-          Number($($el.children().get(1)).text().replace("$", "")) ?? undefined,
-        price:
-          Number($($el.children().get(2)).text().replace("$", "")) ?? undefined,
-        capacity: $($el.children().get(3)).text(),
-        warranty: $($el.children().get(4)).text() || undefined,
-        formFactor: $($el.children().get(5)).text(),
-        technology: $($el.children().get(6)).text(),
-        condition: $($el.children().get(7)).text(),
-      };
-    });
-
+  const prices = await scrapePrices();
   console.log("prices", prices);
 };
 
